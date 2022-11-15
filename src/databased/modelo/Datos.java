b179d7d8792de5c89cb427fb91c8f0584177c9af
Antoni Moragues Font <amoraguesfo@uoc.edu>
2022-11-15 15:03:57 +0100

    Conexion y DAO articulos

--- src/databased/modelo/Datos.java
+++ src/databased/modelo/Datos.java
@@ -1,17 +1,26 @@
 package databased.modelo;
 
+import databased.dao.ArticuloDAO;
+
 import java.util.List;
 
 //el controlador solo llamará a los métodos de esta clase.
 public class Datos {
     private final ListaClientes clientes;
-    private final ListaArticulos articulos;
+    //private final ListaArticulos articulos;
     private final ListaPedidos pedidos;
 
+    private final ArticuloDAO articuloDAO;
+
     public Datos() {
+
         this.clientes = new ListaClientes();
-        this.articulos = new ListaArticulos();
+        //this.articulos = new ListaArticulos();
         this.pedidos = new ListaPedidos();
+
+        this. articuloDAO = new ArticuloDAO();
+
+
     }
 
     public List<Cliente> getClientes() {
@@ -31,15 +40,21 @@ public class Datos {
     }
 
     public List<Articulo> getArticulos() {
-        return articulos.getArrayList();
+        //return articulos.getArrayList();
+        return articuloDAO.readAll();
     }
 
     public Articulo getArticuloByCodigo(String codigo) {
-        return articulos.getArticuloByCodigo(codigo);
+        //return articulos.getArticuloByCodigo(codigo);
+        return articuloDAO.read(codigo);
     }
 
     public boolean addArticulo(Articulo articulo){
-        return articulos.add(articulo);
+        //return articulos.add(articulo);
+        if(articuloDAO.read(articulo.getCodigo()) == null) {
+            return articuloDAO.create(articulo);
+        }
+        return false;
     }
 
     public List<Pedido> getPedidos() {
--- src/databased/vista/OnlineStore.java
+++ src/databased/vista/OnlineStore.java
@@ -1,8 +1,14 @@
 package databased.vista;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class OnlineStore {
+
     public static void main(String[] args){
-        GestionOS gestion = new GestionOS();
-        gestion.inicio();
+
+       GestionOS gestion = new GestionOS();
+       gestion.inicio();
     }
 }
