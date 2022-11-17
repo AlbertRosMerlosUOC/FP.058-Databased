f910ead10e081fc3fdc84200108ebb474e49a26e
iortizfe <iortizfe@uoc.edu>
2022-11-17 04:33:53 +0100

    DAO Exception

--- src/databased/interfaces/InterfaceDAO.java
+++ src/databased/interfaces/InterfaceDAO.java
@@ -2,10 +2,12 @@ package databased.interfaces;
 
 import java.util.List;
 
+import databased.dao.DAOException;
+
 public interface InterfaceDAO<T, K> {
-    public boolean create(T t);
-    public boolean update (T t);
-    public boolean delete(K id);
-    public T read(K id);
-    public List<T> readAll();
+    public boolean create(T t) throws DAOException;
+    public boolean update (T t) throws DAOException;
+    public boolean delete(K id) throws DAOException;
+    public T read(K id) throws DAOException;
+    public List<T> readAll() throws DAOException;
 }
