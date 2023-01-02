eff792a4fe145dd0f7b53f46c171f2c870160a9a
Antoni Moragues Font <amoraguesfo@uoc.edu>
2023-01-02 18:17:19 +0100

    Clean to non module JavaFX project

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module databased.javafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires jakarta.persistence;
-
-    exports databased.javafx;
-    opens databased.javafx to javafx.fxml;
-}
