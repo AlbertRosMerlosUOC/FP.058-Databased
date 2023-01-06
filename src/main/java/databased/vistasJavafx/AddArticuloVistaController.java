4ab13c565cb4f058b6f5b683b533cbe45329128b
Antoni Moragues <amoraguesfo@uoc.edu>
2023-01-06 21:25:55 +0100

    Refractor with modals

---
+++ src/main/java/databased/vistasJavafx/AddArticuloVistaController.java
@@ -0,0 +1,57 @@
+package databased.vistasJavafx;
+
+import databased.MainApp;
+import databased.modelo.Articulo;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class AddArticuloVistaController {
+
+    @FXML
+    private TextField tCodigo;
+    @FXML
+    private TextField tDescripcion;
+    @FXML
+    private TextField tPrecioVenta;
+    @FXML
+    private TextField tGastosEnvio;
+    @FXML
+    private TextField tTiempoPreparacion;
+
+    @FXML
+    private Button btnAdArticulo;
+    @FXML
+    private Button btnCancelAddArticulo;
+
+    private MainApp mainApp;
+    private Stage dialogStage;
+
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+    }
+
+    @FXML
+    public void addArticulo(){
+        if(!tCodigo.getText().equals("")) {
+            Articulo articulo = new Articulo(tCodigo.getText(),
+                    tDescripcion.getText(),
+                    Double.parseDouble(tPrecioVenta.getText()),
+                    Double.parseDouble(tGastosEnvio.getText()),
+                    Integer.parseInt(tTiempoPreparacion.getText()));
+
+            mainApp.getDatos().addArticulo(articulo);
+            dialogStage.close();
+        }
+    }
+
+    @FXML
+    public void setBtnCancelAddArticulo(){
+        dialogStage.close();
+    }
+}
