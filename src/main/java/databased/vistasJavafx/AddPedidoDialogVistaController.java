c97a6b84f29bca9de883a2c8c92f495a88639020
Antoni Moragues <amoraguesfo@uoc.edu>
2023-01-07 16:52:02 +0100

    Añadir filtro tipo cliente

---
+++ src/main/java/databased/vistasJavafx/AddPedidoDialogVistaController.java
@@ -0,0 +1,11 @@
+package databased.vistasJavafx;
+
+import javafx.event.ActionEvent;
+
+public class AddPedidoDialogVistaController {
+    public void addPedido(ActionEvent actionEvent) {
+    }
+
+    public void cancelAddPedido(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/databased/vistasJavafx/PedidosVistaController.java
@@ -0,0 +1,8 @@
+package databased.vistasJavafx;
+
+import javafx.event.ActionEvent;
+
+public class PedidosVistaController {
+    public void showAddPedido(ActionEvent actionEvent) {
+    }
+}
