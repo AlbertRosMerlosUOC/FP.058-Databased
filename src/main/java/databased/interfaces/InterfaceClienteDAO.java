0fc560be8290d0ac1e43d101ad40e5c3d0d8de59
Antoni Moragues <amoraguesfo@uoc.edu>
2023-01-08 16:45:26 +0100

    Clean main

--- src/main/java/databased/interfaces/InterfaceClienteDAO.java
+++
@@ -1,9 +0,0 @@
-package databased.interfaces;
-
-import databased.modelo.Cliente;
-
-import java.util.List;
-
-public interface InterfaceClienteDAO<T, K> extends InterfaceDAO<T, K>{
-    public List<T> readByTipoCliente(String type);
-}
--- src/main/java/databased/interfaces/InterfacePedidoDAO.java
+++
@@ -1,5 +0,0 @@
-package databased.interfaces;
-
-public interface InterfacePedidoDAO<T, K> extends InterfaceDAO<T, K>{
-    public int controlDelete(int numPedido);
-}
